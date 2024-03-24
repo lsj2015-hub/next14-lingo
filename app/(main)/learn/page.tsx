0cafe49f7ab3f6644ef2a9c0b6139a719842d86d
lsj2015-hub <lsj2015@hotmail.com>
2024-03-24 20:48:38 +0800

    Lesson Button

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -6,6 +6,7 @@ import { StickyWrapper } from '@/components/sticky-wrapper';
 import { UserProgress } from '@/components/user-progress';
 
 import { Header } from './header';
+import { Unit } from './unit';
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
@@ -34,7 +35,15 @@ const LearnPage = async () => {
         <Header title={userProgress.activeCourse.title} />
         {units.map((unit) => (
           <div key={unit.id} className="mb-10">
-            {JSON.stringify(unit)}
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
           </div>
         ))}
       </FeedWrapper>
--- db/queries.ts
+++ db/queries.ts
@@ -3,7 +3,14 @@ import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
 
 import db from '@/db/drizzle';
-import { challenges, courses, lessons, units, userProgress } from '@/db/schema';
+import {
+  challengeProgress,
+  challenges,
+  courses,
+  lessons,
+  units,
+  userProgress,
+} from '@/db/schema';
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -23,12 +30,14 @@ export const getUserProgress = cache(async () => {
 });
 
 export const getUnits = cache(async () => {
+  const { userId } = await auth();
   const userProgress = await getUserProgress();
 
-  if (!userProgress?.activeCourseId) {
+  if (!userId || !userProgress?.activeCourseId) {
     return [];
   }
 
+  // TODO: Confirm whether order is needed
   const data = await db.query.units.findMany({
     where: eq(units.courseId, userProgress.activeCourseId),
     with: {
@@ -36,7 +45,9 @@ export const getUnits = cache(async () => {
         with: {
           challenges: {
             with: {
-              challengeProgress: true,
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
             },
           },
         },
